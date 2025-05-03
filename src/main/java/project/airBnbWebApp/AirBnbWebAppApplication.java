7aa27c9819143895c535adc91570343a88c0270a
Ganesh Lonare <ganeshlonare311@gmail.com>
2025-05-03 12:58:48 +0530

    Initialize Project

---
+++ src/main/java/project/airBnbWebApp/AirBnbWebAppApplication.java
@@ -0,0 +1,13 @@
+package project.airBnbWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirBnbWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirBnbWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/airBnbWebApp/AirBnbWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package project.airBnbWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirBnbWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
